044ce061b4cf90fa170460d9c25d5239cbb49733
Juan Galarce <j6alarce@gmail.com>
2021-10-18 10:52:47 -0300

    Se hace el llamado a la api de unsplash y se guardar los resultados en el state photos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Formik, Form, Field } from "formik";
-import "./header.css"
+import "./header.css";
 
 const App = () => {
+  const [photos, setPhotos] = useState([]);
+  console.log(photos);
   return (
     <div>
       <header>
         <Formik
           initialValues={{ search: "" }}
           onSubmit={async (values) => {
+            const response = await fetch(
+              `https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
+              {
+                headers: {
+                  Authorization:
+                    "Client-ID _FYW5gs2njCfUtuItIqKgI-lgPZ6XspaZU7_gD8bkuw",
+                },
+              }
+            );
+            const data = await response.json();
             // Llamar a la api de unsplash
-            console.log(values);
+            setPhotos(data.results);
           }}
         >
           <Form>
